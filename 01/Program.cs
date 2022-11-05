31802b1f4b4e1666718ddd1cca79cbb2c60441cd
Kot <kotvladimir89@mail.ru>
2022-11-05 19:23:39 +0500

    add 01, 04

---
+++ 01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введи цифру a  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи цифру b  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("Является квадратом b");
+}
+else if ( a != b * b)
+{
+    Console.WriteLine("Не является квадратом b");
+}
---
+++ 04/Program.cs
@@ -0,0 +1,21 @@
+﻿// 04. Найти максимальное из трех чисел
+
+Console.Write("Введи цифру a  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи цифру a  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи цифру a  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+if (c > b)
+{
+    max = c;
+}
+
+Console.Write("max = " + max);
